33be4b07306e00b1515f1f789880ba3cd185bbc9
timestep <jbosshellokitty@gmail.com>
2013-07-05 12:47:17 -0400

    more testing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,9 +7,20 @@ describe Product do
 		price_in_cents: 1000000) }
 
   subject{ @product }
-
-  it { should respond_to(:description) }
-  it { should respond_to(:price_in_cents)	}
-  it { should respond_to(:name) }
   
+  describe Description do 
+	  it { should respond_to(:description) }
+	  it { should be_a(text) }
+	end
+
+	describe Price In Cents do 
+		it { should be_a(Integer) }
+	  it { should respond_to(:price_in_cents)	}
+	end
+
+	describe Name do
+		it { should respond_to(:name) }
+		it { should respond_to(String) }
+	end
+
 end
