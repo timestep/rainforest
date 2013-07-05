8a97c8914b60c0ab56a5de198f06ca58261b5de8
timestep <jbosshellokitty@gmail.com>
2013-07-05 17:22:09 -0400

    adding validations to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ActiveRecord::Base
+	attr_accessible :description, :name, :price_in_cents, :id
+
+	validates :description, :name, :presence => true
+	validates :price_in_cents, :numericality => {:only_integer => true}
+	validates :name, uniqueness: true
 end
