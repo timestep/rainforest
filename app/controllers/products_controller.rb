2bd35fe60105980e16667d44d2fb3c07a2c31476
timestep <jbosshellokitty@gmail.com>
2013-07-05 17:01:39 -0400

    json and html response in controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,14 +2,29 @@ class ProductsController < ApplicationController
   
   def index
   	@products = Product.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @products }
+    end
   end
 
   def show
   	@product = Product.find(params[:id])
+    
+      respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @product }
+    end
   end
 
   def new
   	@product = Product.new
+
+      respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @product }
+    end
   end
 
   def edit
@@ -19,26 +34,39 @@ class ProductsController < ApplicationController
   def create
   	@product = Product.new(product_params)
 
-  	if @product.save
-  		redirect_to products_url
-  	else
-  		render :new
-  	end
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render json: @product, status: :created, location: @product }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
   	@product = Product.find(params[:id])
 
-  	if @product.update_attributes(params[:product])
-  		redirect_to product_path(@product)
-  	else
-  		render	:edit
-  	end
+    respond_to do |format|
+      if @product.update_attributes(params[:product])
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
   	@product = Product.find(params[:id])
   	@product.destroy
+
+     respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
+    end  
   end
 
   private
