6e6e4585254f8d3a2c29745d564d16a3f0c92b6d
timestep <jbosshellokitty@gmail.com>
2013-07-05 17:28:31 -0400

    removed attr in product due to rails4

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,4 @@
 class Product < ActiveRecord::Base
-	attr_accessible :description, :name, :price_in_cents, :id
-
 	validates :description, :name, :presence => true
 	validates :price_in_cents, :numericality => {:only_integer => true}
 	validates :name, uniqueness: true
