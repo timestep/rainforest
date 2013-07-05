97c9a624d0b36ac89bfa240c33b61d9c8ea4c4a1
timestep <jbosshellokitty@gmail.com>
2013-07-05 17:58:18 -0400

    added create method for session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	user = User.find_by_email(params[:email])
+  	if user && user.authenticate(params[:password])
+  		session[:user_id] = user.user_id
+  		redirect_to products_url, :notice => "Logged in!"
+  	else
+  		render "new"
+  	end
+  	
   end
 
   def destroy
