c2c2cfaa383544a23bd9db0b2b8fd63e35bf64e6
timestep <jbosshellokitty@gmail.com>
2013-07-05 18:37:53 -0400

    added review model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
 	validates :description, :name, :presence => true
 	validates :price_in_cents, :numericality => {:only_integer => true}
 	validates :name, uniqueness: true
+	has_many 	:reviews
 
 	def formatted_price
 		price_in_dollars = price_in_cents.to_f/100
