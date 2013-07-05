f8965d902950f5482d4839f031342fca36d17570
timestep <jbosshellokitty@gmail.com>
2013-07-05 18:12:06 -0400

    fixed typo in sessions create controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
   	user = User.find_by_email(params[:email])
   	if user && user.authenticate(params[:password])
-  		session[:user_id] = user.user_id
+  		session[:user_id] = user.id
   		redirect_to products_url, :notice => "FIGHT!"
   	else
   		render "new"
