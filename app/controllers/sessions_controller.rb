f2943a801e1f4ab7dc12587e7be558367f0d5faf
timestep <jbosshellokitty@gmail.com>
2013-07-05 18:01:48 -0400

    added destroy method for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,15 @@ class SessionsController < ApplicationController
   	user = User.find_by_email(params[:email])
   	if user && user.authenticate(params[:password])
   		session[:user_id] = user.user_id
-  		redirect_to products_url, :notice => "Logged in!"
+  		redirect_to products_url, :notice => "FIGHT!"
   	else
   		render "new"
   	end
-  	
+
   end
 
   def destroy
+  	session[:user_id] = nil
+  	redirect_to products_url, :notice => "GAME OVER"
   end
 end
