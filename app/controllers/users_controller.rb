042c3af1e15d38fcc9c0ae9f164eea8cc36a3d98
timestep <jbosshellokitty@gmail.com>
2013-07-05 17:43:58 -0400

    modified controller permissions for rails4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,17 @@ class UsersController < ApplicationController
   end
 
   def create
-  	@user = User.new(params[:user])
+  	@user = User.new(user_params)
   	if @user.save
   		redirect_to products_url, :notice => "Signed Up!"
   	else
   		render "new"
   	end	
+  end
+
+  private 
+
+  def user_params
+  	params.require(:user).permit!
   end
 end
