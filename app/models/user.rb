8760de20f6dbea1efdd604f58741cd82646cf2b1
timestep <jbosshellokitty@gmail.com>
2013-07-05 21:29:15 -0400

    added name to form in users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	validates_presence_of :password, :on => :create
+	validates_presence_of :name
 	has_many :reviews
 	has_many :products, :through => :reviews
 end
