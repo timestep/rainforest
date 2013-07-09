b5ed4cc6e7bf76b567a48cc78d0bb20749850483
timestep <jbosshellokitty@gmail.com>
2013-07-09 15:34:26 -0400

    fixedbroken review test

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,10 +6,10 @@ describe Review do
   end
 
   it "has to have a description" do
-  	FactoryGirl.create(:review, comment: nil ).should_not be_valid
+  	FactoryGirl.build(:review, comment: nil ).should_not be_valid
   end
 
   it{ should belong_to(:user) }
   it{ should belong_to(:product) }
-  
+
 end
